2036029b3080992622b4d0706d89434fa41353f0
BalajiSundar28 <94917311+BalajiSundar28@users.noreply.github.com>
2021-12-12 08:18:35 +0530

    Create area& circumferenceofcircle.py

---
+++ area& circumferenceofcircle.py
@@ -0,0 +1,7 @@
+area=0
+circumferance=0
+r=float(input('enter radius'))
+area=3.14*r*r
+circumferance=2*3.14*r
+print('area of circle with radius',r,'is',area,'m^2')
+print('circumferance of circle with radius',r,'is',circumferance,'m')
