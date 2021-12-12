dbd5d12dbc0c5281a97ded138abbf670fa207bc6
BalajiSundar28 <94917311+BalajiSundar28@users.noreply.github.com>
2021-12-12 08:59:27 +0530

    Create daysinamonth.py

---
+++ daysinamonth.py
@@ -0,0 +1,24 @@
+m=input('enter month name')
+
+if m.lower() in ['january','march','may','august','october','december']:
+	
+	print('31 days')
+	
+elif m.lower() in ['april','june','july','september','november']:
+	
+	print('30 days')
+	
+elif m.lower() in ['february']:
+	
+	ask=input('is it a leap year or not(y/n)')
+	
+	if ask.lower()=='y':
+		
+		print('29 days')
+		
+	else:
+		
+		print('28 days')
+		
+else:
+	
